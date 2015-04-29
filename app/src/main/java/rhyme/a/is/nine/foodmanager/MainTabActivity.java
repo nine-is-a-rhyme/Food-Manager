074270f1dac2ba9516cccc5c0512fd9056d3c8c1
Fabio W <fabio.wallner@student.tugraz.at>
2015-04-29 13:23:54 +0200

    [FW, JW] fix

--- app/src/main/java/rhyme/a/is/nine/foodmanager/MainTabActivity.java
+++ app/src/main/java/rhyme/a/is/nine/foodmanager/MainTabActivity.java
@@ -81,46 +81,6 @@ public class MainTabActivity extends ActionBarActivity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        Context context = getApplicationContext();
-        CharSequence text;
-        int duration = Toast.LENGTH_SHORT;
-
-        int id = item.getItemId();
-
-
-        switch (id) {
-            case R.id.action_add:
-                new IntentIntegrator(this).initiateScan();
-                return true;
-            case R.id.action_edit:
-                text = "Edit clicked!";
-                Toast.makeText(context, text, duration).show();
-                return true;
-            case R.id.action_delete:
-                text = "Delete clicked!";
-                Toast.makeText(context, text, duration).show();
-                return true;
-            case R.id.action_settings:
-                text = "Settings clicked!";
-                Toast.makeText(context, text, duration).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
     public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
         private Fragment mFragment;
         private final Activity mActivity;
