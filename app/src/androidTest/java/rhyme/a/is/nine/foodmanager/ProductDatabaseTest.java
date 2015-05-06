052c1184223772fd493409fcba8cd7d1da457f1f
Martin Maritsch <martin.maritsch@student.tugraz.at>
2015-05-06 16:38:57 +0200

    [DL, MM] adapted Testcases for new entity design

--- app/src/androidTest/java/rhyme/a/is/nine/foodmanager/ProductDatabaseTest.java
+++ app/src/androidTest/java/rhyme/a/is/nine/foodmanager/ProductDatabaseTest.java
@@ -5,7 +5,6 @@ import android.test.ApplicationTestCase;
 
 import rhyme.a.is.nine.foodmanager.database.ProductDatabase;
 import rhyme.a.is.nine.foodmanager.product.Product;
-import rhyme.a.is.nine.foodmanager.product.ProductPlace;
 
 
 /**
@@ -15,25 +14,55 @@ public class ProductDatabaseTest extends ApplicationTestCase<Application> {
     public ProductDatabaseTest() { super(Application.class); }
 
     public void testAddProduct() {
-        ProductDatabase.addProduct(new Product("name", "category", "barcode", "size", 1));
-        assertEquals(1, ProductDatabase.getAllProducts().size());
+        ProductDatabase db = new ProductDatabase(null);
+        db.addProduct(new Product("name", "category", "barcode", "size", 1));
+        assertEquals(1, db.getAllProducts().size());
     }
 
     public void testAddProductShouldNotReturnProducts() {
-        ProductDatabase.addProduct(new Product("name", "category", "barcode", "size", 1));
-        assertEquals(0, ProductDatabase.getAllProducts().size());
+        ProductDatabase db = new ProductDatabase(null);
+        db.addProduct(new Product("name", "category", "barcode123", "size", 1));
+        assertEquals("barcode123", db.getProductByPosition(0).getBarcode());
     }
 
     public void testGetProductByName() {
-        ProductDatabase.addProduct(new Product("name123", "category123", "barcode", "size", 1));
-        assertEquals("category123", ProductDatabase.getProductByName("name123").getCategory());
+        ProductDatabase db = new ProductDatabase(null);
+        db.addProduct(new Product("name123", "category123", "barcode", "size", 1));
+        assertEquals("category123", db.getProductByName("name123").getCategory());
     }
 
     public void testModifyProduct() {
-        ProductDatabase.addProduct(new Product("name456", "category123", "barcode", "size", 1));
-        Product product = ProductDatabase.getProductByName("name456");
+        ProductDatabase db = new ProductDatabase(null);
+        db.addProduct(new Product("name456", "category123", "barcode", "size", 1));
+        Product product = db.getProductByName("name456");
         product.setSize("12345");
-        assertEquals("12345", ProductDatabase.getProductByName("name456").getSize());
+        assertEquals("12345", db.getProductByName("name456").getSize());
     }
+
+    public void testDecreaseProductByOne() {
+        ProductDatabase db = new ProductDatabase(null);
+        db.addProduct(new Product("name456", "category123", "barcode", "size", 2));
+        Product product = db.getProductByName("name456");
+        product.decreaseCount();
+        assertEquals(1, db.getProductByName("name456").getCount());
+    }
+
+    public void testDecreaseProductShouldDeleteProductCompletely() {
+        ProductDatabase db = new ProductDatabase(null);
+        db.addProduct(new Product("testname", "category123", "barcode12345", "size", 1));
+        db.removeProductByPosition(0, true);
+        assertNull(db.getProductByPosition(0));
+    }
+
+    public void testSaveAndReadDb() {
+        ProductDatabase db = new ProductDatabase("test.db");
+        db.addProduct(new Product("name456", "category123", "thisIsAFakeBarcode", "size", 1));
+        db.writeToFile(getSystemContext());
+
+        ProductDatabase newDb = new ProductDatabase("test.db");
+        newDb.readFromFile(getSystemContext());
+        assertEquals("thisIsAFakeBarcode", newDb.getProductByName("name456").getBarcode());
+    }
+
 
 }
